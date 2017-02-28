cb22dcf89c91341501313a0b9c8e9a6f414ab0bd
WinnieRotich <winniecherop@outlook.com>
2017-02-28 11:43:13 +0300

    Update sum_of_an_array.py

--- issue-001/sum_of_an_array.py
+++ issue-001/sum_of_an_array.py
@@ -1,13 +1,9 @@
 import sys
-
 #enter the size of the array
 n = int(raw_input().strip())
-
 #enter the array's elements
 array = map(int,raw_input().strip().split(' '))
-#
 sum = 0
-
 #using a loop
 for i in array:
     sum += i
